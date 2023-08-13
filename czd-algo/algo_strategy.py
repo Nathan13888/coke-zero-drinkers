fff54d7125ddb498750600359c4d2882354c26bc
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2023-08-13 20:12:04 +0000

    implement spam offense strategy
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- czd-algo/algo_strategy.py
+++ czd-algo/algo_strategy.py
@@ -393,42 +393,66 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
 
         # TUNABlES
-        detected_enemy_units_threshold = 6
-        stall_with_interceptors_until_turn = 5
+        detected_enemy_units_threshold = 8
+        stall_turns = 5
 
-        # TODO: get budget
+        mobile_budget = game_state.get_resources()[1]
+        opponent_sp = game_state.get_resources(player_index=1)[0]
+        opp_sp_min_threshold = 8
 
         ######################################
 
         # If the turn is less than 5, stall with interceptors and wait to see enemy's base
-        if game_state.turn_number < stall_with_interceptors_until_turn:
-            self.stall_with_interceptors(game_state)
-            return
-        
-        # TODO: consider all paths to spawn
-        if (
-            # detect units on the left side (top-left quadrant)
-            self.detect_enemy_unit(
-                game_state, unit_type=None,
-                valid_x=[x for x in range(14)],
-                valid_y=[y+14 for y in range(14)],
-            )
-            > detected_enemy_units_threshold
-        ):
+        if game_state.turn_number < stall_turns:
+            # self.stall_with_interceptors(game_state)
             # "quick ping" - catch 'em off guard
             # TODO: check if this works
             
             random_locations = [[13, 0], [14, 0], [0, 13], [27, 13]]
             random_location = random.choice(random_locations)
             game_state.attempt_spawn(SCOUT, random_location, 1000)
+            return
+        
+
+        # TODO: consider all paths to spawn
+        mp_spawn_location = [14+12, 12]
+
+        if ( # they are vulnerable??
+            # if they have fewer than the threshold number of structures
+            self.detect_enemy_unit(
+                game_state, unit_type=None,
+                valid_x=[x for x in range(14)],
+                valid_y=None,
+            )
+            # detect units on the left side (top-left quadrant)
+            # self.detect_enemy_unit(
+            #     game_state, unit_type=None,
+            #     valid_x=[x for x in range(14)],
+            #     valid_y=[y+14 for y in range(14)],
+            # )
+            > detected_enemy_units_threshold
+            or opponent_sp < opp_sp_min_threshold
+        ):
 
             # TODO: sort out demolisher_line_strategy
             # self.demolisher_line_strategy(game_state)
+            # TODO: "stage" an attack, require support, place intercepters in front?
+            game_state.attempt_spawn(SCOUT, mp_spawn_location, 1000)
+
         else:
+
+            # TODO: figure out where to spawn scouts + how many
+            # game_state.config["unitInformation"][5]["cost2"]
+            cost_of_intercepter = 2 # TODO: get from config
+            saved_mp = 5
+            count = int((mobile_budget - saved_mp) / cost_of_intercepter) # max number of scouts to spawn per push
+            
+            game_state.attempt_spawn(INTERCEPTOR, mp_spawn_location, count)
+
             # They don't have many units in the front so lets figure out their least defended area and send Scouts there.
             # - spawn scouts every odd turn
             # - spam to protect self
-            if game_state.turn_number % 2 == 1:
+            # if game_state.turn_number % 2 == 1:
                 # To simplify we will just check sending them from back left and right
                 # scout_spawn_location_options = [[13, 0], [14, 0]]
                 # best_location = self.least_damage_spawn_location(
@@ -436,15 +460,6 @@ class AlgoStrategy(gamelib.AlgoCore):
                 # )
                 # game_state.attempt_spawn(SCOUT, best_location, 1000)
 
-                # TODO: figure out where to spawn scouts + how many
-                count = 15 # max number of scouts to spawn per push
-                location = [14+12, 12]
-                game_state.attempt_spawn(SUPPORT, location, 1)
-                game_state.attempt_spawn(SCOUT, location, count)
-
-            # Lastly, if we have spare SP, let's build some supports
-            support_locations = [[13, 2], [14, 2], [13, 3], [14, 3]]
-            game_state.attempt_spawn(SUPPORT, support_locations)
 
 
 #######################################################################################################################
